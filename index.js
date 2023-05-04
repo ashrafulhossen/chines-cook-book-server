c9937a6168e12f5282220e06d813de0f10c049b2
ashrafulhossen <b.asrafulhossencht@gmail.com>
2023-05-04 18:48:24 +0600

    setup chefs routes

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const port = process.env.PORT || 5000;
 const chefs = require("./data/chefs.json");
 const recipes = require("./data/recipes.json");
 
+app.use(cors());
+
 app.get("/", (req, res) => {
 	res.send("Chinese CookBook Server is running");
 });
+
+app.get("/recipes", (req, res) => {
+    res.send(recipes);
+});
+
+app.get("/chefs", (req, res) => {
+    res.send(chefs);
+})
+
 
 app.listen(port, () => {
     console.log("Chinese cookbook server is running.....")
